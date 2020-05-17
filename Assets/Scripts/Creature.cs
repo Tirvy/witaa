f36ce573f1b47f09a38725d68685d26bcffe14eb
Ernest Gainullin <8tirvy@gmail.com>
2020-05-17 16:27:15 +0300

    Ravager death added

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -6,16 +6,28 @@ public class Creature : MonoBehaviour
 {
 	
 	public int maxHealth = 60;
+	public float deathSpeed = 1;
+	private Material material;
+	public float deathProgress = 0;
 	private int health;
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+		material = GetComponent<SpriteRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+		if (health <= 0) {
+			deathProgress += deathSpeed * Time.deltaTime;
+			material.SetFloat("_Progress", deathProgress);
+			if (deathProgress > 1) {
+				Debug.Log("Creature killed");
+				Destroy(gameObject);
+			}
+		}
         
     }
 	
@@ -24,7 +36,6 @@ public class Creature : MonoBehaviour
 		health -= damage;
 		if (health <= 0) 
 		{
-			Destroy(gameObject);
 		}
 	}
 }
